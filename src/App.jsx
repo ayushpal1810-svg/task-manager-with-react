3cafe06765c8a3dc7be1e6b6bbef7a8efd11530e
ayushpal1810-svg <ayushpal1810@gmail.com>
2026-06-16 14:37:16 +0530

    added addtask,edit,delete task feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
+  const inputRef = useRef(null);
   const [title, settitle] = useState("")
   const [task, settask] = useState([])
+  const [editId, setEditId] = useState(null);
+  const pendingTasks = task.filter((t) => !t.completed);
+const completedTasks = task.filter((t) => t.completed);
   const addtask = ()=>{
     if (!title.trim()) return;
     const newtask={
@@ -14,20 +20,84 @@ function App() {
       title: title,
       completed: false
     };
+    console.log(title)
     settask([...task,newtask]);
     settitle("")
   };
-   console.log(title)
-    console.log(task)
+  const completedtask=(id)=>{
+    settask(
+      task.map((t)=>
+      t.id===id ? { ...t, completed: !t.completed }: t
+
+      )
+    )
+  }
+  const deletetask=(id)=>{
+    settask(
+      task.filter((t)=> t.id!==id
+      )
+    )
+  }
+ const edittask = (id) => {
+  const taskToEdit = task.find((t) => t.id === id);
+
+  settitle(taskToEdit.title);  
+  setEditId(id);   
+  inputRef.current.focus();
+};
+const saveTask = () => {
+  settask(
+    task.map((t) =>
+      t.id === editId
+        ? { ...t, title: title }
+        : t
+    )
+  );
+
+  settitle("");
+  setEditId(null);
+};
+const cancelEdit = () => {
+  setEditId(null);
+  settitle("");
+};
+   
   return (
     <>
     <input type="text" value={title}
+    ref={inputRef}
     onChange={(e)=> settitle(e.target.value)}
     placeholder='enter task'
+    onKeyDown={(e) => {
+  if (e.key === "Enter") {
+    editId ? saveTask() : addtask();
+  }
+  if(e.key==='Escape') cancelEdit();
+}
+
+    }
     />
-    <button onClick={addtask}>add task</button>
-   { task.map ((t)=>(
-    <div key={t.id}>{t.title}</div>
+    <button onClick={editId ? saveTask: addtask }>{editId ? "save task" : "add task"}</button>
+         {editId && (
+  <button onClick={cancelEdit}>
+    Cancel
+  </button>
+)}
+    <h3>Pending task</h3>
+   { pendingTasks.map ((t)=>(
+    <div>
+    <span key={t.id} onClick={()=>completedtask(t.id) } className={t.completed ? "done" : ""}>{t.title} 
+    
+     </span>
+     <button onClick={()=>deletetask(t.id)}>Delete</button>
+     <button onClick={()=>edittask(t.id)}>Edit</button>
+
+     
+     </div>
+   ))}
+   <h3>completed task</h3>
+   { completedTasks.map ((t)=>(
+    <div key={t.id} onClick={()=>completedtask(t.id) } className={t.completed ? "done" : ""}>{t.title}</div>
    ))}
     </>
   )
