b2c9913b762053ff605471a63d04f07966a65eee
ayushpal1810-svg <ayushpal1810@gmail.com>
2026-06-23 19:57:01 +0530

    completed front end and added local storage and finally going to start backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useRef } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -10,10 +11,19 @@ function App() {
   const inputRef = useRef(null);
   const [dueDate, setDueDate] = useState("");
   const [title, settitle] = useState("")
-  const [task, settask] = useState([])
+  const [task, settask] = useState(() => {
+  const saved = localStorage.getItem("tasks");
+  return saved ? JSON.parse(saved) : [];
+});
+useEffect(() => {
+  localStorage.setItem("tasks", JSON.stringify(task));
+}, [task]);
   const [editId, setEditId] = useState(null);
   const [category, setcategory] = useState("")
   const [taskstate, settaskstate] = useState("all")
+  const [filtercategory, setfiltercategory] = useState("all")
+  const [dateFilter, setdateFilter] = useState("all");
+const [customDate, setCustomDate] = useState("");
   const pendingTasks = task.filter((t) => !t.completed);
   const completedTasks = task.filter((t) => t.completed);
   const addtask = () => {
@@ -53,6 +63,9 @@ function App() {
     const taskToEdit = task.find((t) => t.id === id);
 
     settitle(taskToEdit.title);
+      setcategory(taskToEdit.category);
+
+  setDueDate(taskToEdit.dueDate);
     setEditId(id);
     inputRef.current.focus();
   };
@@ -68,13 +81,14 @@ function App() {
 
       task.map((t) =>
         t.id === editId
-          ? { ...t, title: title }
-          : t
+          ? { ...t, title: title, category: category , dueDate:dueDate }: t
       )
     );
 
     console.log(title)
-    settitle("");
+    setcategory("")
+    settitle("")
+    setDueDate("")
     setEditId(null);
 
   };
@@ -82,7 +96,39 @@ function App() {
     setEditId(null);
     settitle("");
   };
+  let filteredTasks = task;
 
+if (taskstate === "pending") {
+  filteredTasks = task.filter((t) => !t.completed);
+}
+
+else if (taskstate === "completed") {
+  filteredTasks = task.filter((t) => t.completed);
+}
+if (filtercategory !== "all") {
+  filteredTasks = filteredTasks.filter(
+    (t) => t.category === filtercategory
+  );
+}
+  const today = new Date().toISOString().split("T")[0];
+ 
+if (dateFilter === "today") {
+
+
+  filteredTasks = filteredTasks.filter(
+    (t) => t.dueDate === today
+  );
+}
+
+if (dateFilter === "custom" && customDate) {
+  filteredTasks = filteredTasks.filter(
+    (t) => t.dueDate === customDate
+  );
+}
+
+const visibleTasks = [...filteredTasks].sort(
+  (a, b) => a.completed - b.completed
+);
   return (
     <>
       <input type="text" value={title}
@@ -104,7 +150,7 @@ function App() {
         <option value="">no category</option>
         <option value="Work">Work</option>
         <option value="Personal">Personal</option>
-        <option value="Study">Study</option>
+       
       </select>
       <input
         type="date"
@@ -118,14 +164,42 @@ function App() {
         </button>
       )}
       <span className='taskstateclass'>
-        <span onClick={()=>settaskstate("all")} className={taskstate === "all" ? "active" : ""}>all</span>
-        <span onClick={()=>settaskstate("pending") } className={taskstate === "pending" ? "active" : ""}>pending</span>
-        <span onClick={()=>settaskstate("completed")} className={taskstate === "completed" ? "active" : ""}>completed</span>
+        <span onClick={()=>settaskstate("all")} className={`box ${taskstate === "all" ? "active" : ""}`}>all</span>
+        <span onClick={()=>settaskstate("pending") } className={`box ${taskstate === "pending" ? "active" : ""}`}>pending</span>
+        <span onClick={()=>settaskstate("completed")} className={`box ${taskstate === "completed" ? "active" : ""}`}>completed</span>
       </span>
-      <h3>Pending task</h3>
-      {pendingTasks.map((t) => (
+        <span className='taskstateclass'>
+        <span onClick={()=>setfiltercategory("all")} className={`box ${filtercategory === "all" ? "active" : ""}`}>all</span>
+        <span onClick={()=>setfiltercategory("Work") } className={`box ${filtercategory === "Work" ? "active" : ""}`}>work</span>
+        <span onClick={()=>setfiltercategory("Personal")} className={`box ${filtercategory === "Personal" ? "active" : ""}`}>personal</span>
+        <span onClick={()=>setfiltercategory("no category")} className={`box ${filtercategory === "no category" ? "active" : ""}`}>no category</span>
+      </span>
+            <span className='taskstateclass'>
+        <span onClick={()=>setdateFilter("all")} className={`box ${dateFilter === "all" ? "active" : ""}`}>all</span>
+        <span onClick={()=>setdateFilter("today") } className={`box ${dateFilter === "today" ? "active" : ""}`}>today</span>
+        <span onClick={()=>setdateFilter("custom")} className={`box ${dateFilter === "custom" ? "active" : ""}`}>custom</span>
+      </span>
+      {dateFilter === "custom" && (
+  <input
+    type="date"
+    value={customDate}
+    onChange={(e) => setCustomDate(e.target.value)}
+  />
+)}
+
+      {visibleTasks.map((t)=>{
+const isOverdue =
+  !t.completed &&
+  t.dueDate &&
+  t.dueDate < today;
+        return(
         <div key={t.id} className={editId === t.id ? "editing" : ""}>
-          <span onClick={() => completedtask(t.id)} className={t.completed ? "done" : ""}>
+
+
+     <span onClick={() => completedtask(t.id)} className={`
+  ${t.completed ? "done" : ""}
+  ${isOverdue ? "overdue" : ""}
+`}>
             {t.title}({t.category})- {t.dueDate}
 
 
@@ -134,18 +208,12 @@ function App() {
           <button onClick={() => edittask(t.id)}>Edit</button>
 
 
-        </div>
-      ))}
-      <h3>completed task</h3>
-      {completedTasks.map((t) => (
-        <div key={t.id} onClick={() => completedtask(t.id)}>
-          <span className={t.completed ? "done" : ""}>
-            {t.title}
-          </span>
+  </div>
+        );
+})}
       
-          ({t.category}) - {t.dueDate}
-        </div>
-      ))}
+
+
       
     </>
   )
